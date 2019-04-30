fcc21ffc60b31c726c7c96e3e8a2cfd5d733e878
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2019-04-30 14:37:09 +0900

    change_route2
    コンフリクトが発生したため修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-root "messages#index"
-resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
+  devise_for :users
+  root "messages#index"
+  resources :users, only: [:edit, :update]
 end
