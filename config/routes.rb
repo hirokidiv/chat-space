a2934863138016bb9c54e4909dd7c75dfb6615de
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2019-04-30 14:30:56 +0900

    change_route
    routeでendタグが足りなかったので修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ root "messages#index"
 resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+  end
 end
