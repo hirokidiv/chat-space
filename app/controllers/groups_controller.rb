532d359fce7eead3751c2bdfd0c071d4f9182418
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2019-05-01 12:33:53 +0900

    create_groups
    chat-spaceのサイドバーの実装。
    作成したグループをサイドバーに表示

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,21 @@
 class GroupsController < ApplicationController
   def create
-    
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
   
-  
+  private
+  def group_params
+    # params.require(:chat_group).permit(:name)
+    params.require(:group).permit(:name, { user_ids: []})
+  end
 end
