856fc1e13dffc5092b424efbb60ef9d24261fe13
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2019-05-02 16:13:37 +0900

    create_groups_edit
    グループの編集機能を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
   def create
     @group = Group.new(group_params)
     if @group.save
@@ -12,10 +13,22 @@ class GroupsController < ApplicationController
     @group = Group.new
     @group.users << current_user
   end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
   
   private
   def group_params
     # params.require(:chat_group).permit(:name)
     params.require(:group).permit(:name, { user_ids: []})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
