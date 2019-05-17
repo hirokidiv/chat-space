4eca6ddef192849db5e5f079a86e2c4aecba2980
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2019-05-17 13:51:51 +0900

    create_message.js
    ajax用のjsコードを作成

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,43 @@
+$(function () {
+  function buildHTML(message){
+    var body = message.body.present ? `${message.body}` : '';
+    var img = message.image ? `<img src=${message.image}>` : '';
+    var html = `<div class="message__upper-info>
+                  <div class="message__upper-info__user">
+                    ${ message.user.name }
+                  </div>
+                  <div class="message__upper-info__date">
+                    ${ message.created_at.strftime("%Y/%m/%d %H:%M") }
+                  </div>
+                </div>
+                <div class="message__Text">
+                  ${ body }
+                </div>
+                <img src=${ img }>
+                `
+    return html
+  }
+  
+  $(".new_message").on("submit", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
+    console.log(url);
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataTypw: "json",
+        processData: false,
+        contentType: false
+      })
+        .done(function(data){
+          var html = buildHTML(data)
+          $(".message").append(html)
+          $(".input-box__text").val("")
+        })
+        .fail(function(){
+          alert("error");
+        })
+  })
+});
