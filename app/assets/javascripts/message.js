e2a9d014be653e6cc0ac1c57925fa5807cd7d82b
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2019-05-28 22:37:48 +0900

    fix_message.js
    非同期通信の確認でエラーが見つかったので修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,29 @@
-$(function () {
-  function buildHTML(message){
-    var body = message.body.present ? `${message.body}` : '';
-    var img = message.image ? `<img src=${message.image}>` : '';
-    var html = `<div class="message__upper-info>
-                  <div class="message__upper-info__user">
-                    ${ message.user.name }
-                  </div>
-                  <div class="message__upper-info__date">
-                    ${ message.created_at.strftime("%Y/%m/%d %H:%M") }
-                  </div>
-                </div>
-                <div class="message__Text">
-                  ${ body }
-                </div>
-                <img src=${ img }>
-                `
-    return html
-  }
-  
-  $(".new_message").on("submit", function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr("action");
-    console.log(url);
+$(document).on('turbolinks:load', function(){ 
+  $(function(){
+    function buildHTML(message){
+      var body = message.body? `${message.body}` : '';
+      var img = message.image? `${message.image}` : '';
+      var html = `<div class="message" data-id="${message.id}">
+                    <div class="message__upper-info">
+                      <div class="message__upper-info__user">
+                        ${message.name}
+                      </div>
+                      <div class="message__upper-info__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message__Text">
+                      ${ body }
+                    </div>
+                    <img src="${ img }">
+                  </div>`
+      return html
+    }
+
+    $("#new_message").on("submit", function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr("action");
       $.ajax({
         url: url,
         type: "POST",
@@ -33,11 +34,15 @@ $(function () {
       })
         .done(function(data){
           var html = buildHTML(data)
-          $(".message").append(html)
-          $(".input-box__text").val("")
+          $(".messages").append(html)
+          $(".input-box__text").val("");
+          $(".image-label__input").val("");
+          $(".new-message__submit-btn").attr("disabled", false);
+          $('.messages').animate({ scrollTop: $(".messages")[0].scrollHeight }, 'fast');
         })
         .fail(function(){
           alert("error");
         })
+    })
   })
 });
